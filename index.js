3b1518ff9bc3d6acd772ad835713c32550aa857e
sgukz <thorapee055@gmail.com>
2019-06-11 11:31:25 +0700

    commit date: 11/06/2562 Test webhook v.2

--- index.js
+++ index.js
@@ -15,29 +15,81 @@ restService.use(
 restService.use(bodyParser.json());
 
 restService.post("/webhook", function (req, res) {
+  let userMessage = req.body.events[0].message.text;
   let userId = req.body.events[0].source.userId;
-  //let msg = JSON.stringify(req.body)
-  reply(userId)
-  res.sendStatus(200)
+  if (userMessage == "ลงทะเบียน") {
+    let formatMessage = {
+      "type": "flex",
+      "altText": "ลงทะเบียน",
+      "contents": {
+        "type": "bubble",
+        "styles": {
+          "header": {
+            "backgroundColor": "#16a9ff"
+          }
+        },
+        "header": {
+          "type": "box",
+          "layout": "vertical",
+          "contents": [
+            {
+              "type": "text",
+              "text": "ลงทะเบียน",
+              "size": "md",
+              "weight": "bold",
+              "color": "#FFFFFF"
+            }
+          ]
+        },
+        "hero": {
+          "type": "image",
+          "url": "https://show-image.herokuapp.com/img/logo_reh.png",
+          "size": "md",
+          "aspectMode": "cover"
+        },
+        "body": {
+          "type": "box",
+          "layout": "vertical",
+          "contents": [
+            {
+              "type": "text",
+              "text": "ลงทะเบียนเพื่อรับข้อมูลข่าวสาร",
+              "align": "center"
+            },
+            {
+              "type": "button",
+              "style": "link",
+              "action": {
+                "type": "uri",
+                "label": "ยืนยันลงทะเบียน",
+                "uri": "http://queueonline.reh.go.th/index.html?userId=" + userId
+              }
+            }
+          ]
+        }
+      }
+
+    }
+    reply(userId, formatMessage)
+    res.sendStatus(200)
+  }
 });
-function reply(msg) {
+
+function reply(userId, formatMessage) {
   let headers = {
-      'Content-Type': 'application/json',
-      'Authorization': 'Bearer {Qg8Oxy7Paskgsph62kxIuKCI91vu/AIBada8UU7x/FY9oGK5ad8y3Xac0BeYZOdQFF3zG6rTTznGdABacEB1AZM4Hi7UaQ5riscja3LjH7NHN2EPh3eKvrNl3ANkyNVPMxAevnBvfvXIB4+KfoPkcwdB04t89/1O/w1cDnyilFU=}'
+    'Content-Type': 'application/json',
+    'Authorization': 'Bearer {Qg8Oxy7Paskgsph62kxIuKCI91vu/AIBada8UU7x/FY9oGK5ad8y3Xac0BeYZOdQFF3zG6rTTznGdABacEB1AZM4Hi7UaQ5riscja3LjH7NHN2EPh3eKvrNl3ANkyNVPMxAevnBvfvXIB4+KfoPkcwdB04t89/1O/w1cDnyilFU=}'
   }
   let body = JSON.stringify({
-      to: "U0ce66a9d268b3f1d81d04b30631acc87",
-      messages: [{
-          type: 'text',
-          text: msg
-      }]
+    to: userId,
+    messages: [formatMessage]
   })
   request.post({
-      url: 'https://api.line.me/v2/bot/message/push',
-      headers: headers,
-      body: body
+    url: 'https://api.line.me/v2/bot/message/push',
+    headers: headers,
+    body: body
   }, (err, res, body) => {
-      console.log('status = ' + res.statusCode);
+    console.log('status = ' + res.statusCode);
   });
 }
 restService.listen(process.env.PORT || 8000, function () {
