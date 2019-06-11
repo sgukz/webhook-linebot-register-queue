7cf3160b4abd12867780147e7632345f597445f4
sgukz <thorapee055@gmail.com>
2019-06-11 10:31:08 +0700

    commit date: 11/06/2562 test fulfillment v.1 test

--- index.js
+++ index.js
@@ -15,12 +15,36 @@ restService.use(bodyParser.json());
 
 restService.post("/webhook", function (req, res) {
   let userId = req.body.originalDetectIntentRequest.payload.data.source.userId;
+  reply(userId)
   res.json({
     fulfillmentText:  userId,
     source: "line"
   });
 });
-
+function reply(userId) {
+  let headers = {
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer {KZFW6ZW36NTo2nWriM/bqpf4B2YmOsKkx0Ijd1Zl6n3oOBo//A2TErMT5oBAv5nKV0/xTLdDVC4vXDzfaufdeXStbBPgDNe4Of9WE/49lPkh00j0LDSEQZmauPnWQL4QAQOkKLIpYUmAfgV0BQSJYgdB04t89/1O/w1cDnyilFU=}'
+  }
+  let body = JSON.stringify({
+      to: userId,
+      messages: [{
+          type: 'text',
+          text: 'Hello'
+      },
+      {
+          type: 'text',
+          text: 'How are you?'
+      }]
+  })
+  request.post({
+      url: 'https://api.line.me/v2/bot/message/push',
+      headers: headers,
+      body: body
+  }, (err, res, body) => {
+      console.log('status = ' + res.statusCode);
+  });
+}
 restService.listen(process.env.PORT || 8000, function () {
   console.log("Server up and listening");
 });
