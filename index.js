f2d3597e262cc929a64fff744e3cb9f6416e2907
sgukz <thorapee055@gmail.com>
2019-06-11 10:37:07 +0700

    commit date: 11/06/2562 Test webhook v.1

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const sharp = require('sharp');
 const bodyParser = require("body-parser");
 const axios = require('axios');
 const moment = require('moment');
+const request = require('request')
 moment.locale('th');
 const restService = express("");
 restService.use(
@@ -14,27 +15,20 @@ restService.use(
 restService.use(bodyParser.json());
 
 restService.post("/webhook", function (req, res) {
-  let userId = req.body.originalDetectIntentRequest.payload.data.source.userId;
-  reply(userId)
-  res.json({
-    fulfillmentText:  userId,
-    source: "line"
-  });
+  let msg = JSON.stringify(req.body)
+  reply(msg)
+  res.sendStatus(200)
 });
-function reply(userId) {
+function reply(msg) {
   let headers = {
       'Content-Type': 'application/json',
-      'Authorization': 'Bearer {KZFW6ZW36NTo2nWriM/bqpf4B2YmOsKkx0Ijd1Zl6n3oOBo//A2TErMT5oBAv5nKV0/xTLdDVC4vXDzfaufdeXStbBPgDNe4Of9WE/49lPkh00j0LDSEQZmauPnWQL4QAQOkKLIpYUmAfgV0BQSJYgdB04t89/1O/w1cDnyilFU=}'
+      'Authorization': 'Bearer {Qg8Oxy7Paskgsph62kxIuKCI91vu/AIBada8UU7x/FY9oGK5ad8y3Xac0BeYZOdQFF3zG6rTTznGdABacEB1AZM4Hi7UaQ5riscja3LjH7NHN2EPh3eKvrNl3ANkyNVPMxAevnBvfvXIB4+KfoPkcwdB04t89/1O/w1cDnyilFU=}'
   }
   let body = JSON.stringify({
-      to: userId,
+      to: "U0ce66a9d268b3f1d81d04b30631acc87",
       messages: [{
           type: 'text',
-          text: 'Hello'
-      },
-      {
-          type: 'text',
-          text: 'How are you?'
+          text: msg
       }]
   })
   request.post({
