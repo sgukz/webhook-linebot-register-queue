4e7e1e5502bef3fb38a0c7aad3b58dd6c3253efc
sgukz <thorapee055@gmail.com>
2019-06-11 09:26:25 +0700

    commit date: 11/06/2562 test4

--- index.js
+++ index.js
@@ -16,11 +16,8 @@ restService.use(express.static(publicDir));
 restService.use(bodyParser.json());
 
 restService.post("/webhook", function (req, res) {
-  let reply_token = req.body
+  let reply_token = req.body.events[0].replyToken
   reply(reply_token)
-  // res.json({
-  //   data: JSON.stringify(reply_token)
-  // })
   res.sendStatus(200)
 });
 
@@ -30,14 +27,18 @@ function reply(reply_token) {
     'Authorization': 'Bearer {Qg8Oxy7Paskgsph62kxIuKCI91vu/AIBada8UU7x/FY9oGK5ad8y3Xac0BeYZOdQFF3zG6rTTznGdABacEB1AZM4Hi7UaQ5riscja3LjH7NHN2EPh3eKvrNl3ANkyNVPMxAevnBvfvXIB4+KfoPkcwdB04t89/1O/w1cDnyilFU=}'
   };
   let body = JSON.stringify({
-    to: "U0ce66a9d268b3f1d81d04b30631acc87",
+    replyToken: reply_token,
     messages: [{
-      "type": "text",
-      "text": JSON.stringify(reply_token)
+      type: 'text',
+      text: 'Hello'
+    },
+    {
+      type: 'text',
+      text: 'How are you?'
     }]
-  });
+  })
   request.post({
-    url: 'https://api.line.me/v2/bot/message/push',
+    url: 'https://api.line.me/v2/bot/message/reply',
     headers: headers,
     body: body
   }, (err, res, body) => {
